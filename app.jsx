c60975781752343f79cf08a03b90246bdf5a7628
mmmmr <ob12564@163.com>
2024-01-14 15:57:30 +0800

    day02

--- app.jsx
+++ app.jsx
@@ -2,7 +2,9 @@ import React from './react/react.js'
 
 const App = () => {
   return <div id="app">
+    <span>
     react-app
+    </span>
   </div>
 }
 
--- react/react.js
+++ react/react.js
@@ -18,22 +18,79 @@ const createElement = (type, props, ...children) => {
   }
 }
 
-const render = (el, container) => {
-  const { type, props } = el
+const createDom = (type) => {
+  return type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type)
+}
 
-  const dom = type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(el.type)
-
+const updateProps = (dom, props) => {
   props && Object.keys(props).forEach(key => {
     if (key !== 'children') {
       dom[key] = props[key]
     }
   })
+}
 
-  props && props.children.forEach(child => {
-    render(child, dom)
+const initChildren = (fiber) => {
+  const { children } = fiber.props
+  let prev = null
+  children.forEach((child, index) => {
+    const newFiber = {
+      dom: child.dom,
+      parent: fiber,
+      child: null,
+      sibling: null,
+      type: child.type,
+      props: child.props
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prev.sibling = newFiber
+    }
+    prev = newFiber
   })
+}
 
-  container.appendChild(dom)
+const perWorkOfUnit = (fiber) => {
+  const { type, props, parent } = fiber
+  // 没有dom创建dom
+  if (!fiber.dom) {
+    fiber.dom = createDom(type)
+    // 更新props
+    updateProps(fiber.dom, props)
+    // 添加节点
+    parent.dom.appendChild(fiber.dom)
+  }
+
+  // 创建链表
+  initChildren(fiber)
+
+  // 返回下一个节点
+  return fiber.child || fiber.sibling
+}
+
+let nextUnitOfWork = null
+let shouldYield = false
+
+const workLoop = (IdleDeadline) => {
+  shouldYield = false
+  while (!shouldYield && nextUnitOfWork) {
+    nextUnitOfWork = perWorkOfUnit(nextUnitOfWork)
+    shouldYield = IdleDeadline.timeRemaining() > 0
+  }
+  requestIdleCallback(workLoop)
+}
+
+// 开始渲染子节点
+requestIdleCallback(workLoop)
+
+const render = (el, container) => {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [el],
+    }
+  }
 }
 
 const React = {
